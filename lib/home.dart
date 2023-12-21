c0e3e005b70963d119b8e9a011d5179576521ade
Anurag Lamsal <vizculus@gmail.com>
2023-12-21 13:28:16 +0545

    created a node backend and successfully communicated with it. for testing purposes, i test sending image to server and the server converts it to grayscale and sends it back. this worked!

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:volume_watcher/volume_watcher.dart';
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+import 'package:http/src/byte_stream.dart';
 
 class MyHomePage extends StatefulWidget {
 	MyHomePage({super.key, required this.title, required this.cameras});
@@ -16,46 +18,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
 	late CameraController controller;
+	//late responseData; 
 
 	@override
 	initState() {
 		super.initState();
-		VolumeWatcher.hideVolumeView = true;
-		controller = CameraController(widget.cameras[0], ResolutionPreset.max);
-		controller.initialize().then((_) {
-			if (!mounted) {
-				return;
-			}
-			setState(() {});
-		}).catchError((Object e) {
-			if (e is CameraException) {
-				switch (e.code) {
-					case 'CameraAccessDenied':
-						// Handle access errors here.
-						break;
-					default:
-						// Handle other errors here.
-						break;
-				}
-			}
-		});
 
-	}
-
-	Future takePicture() async {
-		if (!controller.value.isInitialized) {return null;}
-		if (controller.value.isTakingPicture) {return null;}
-		try {
-			await controller.setFlashMode(FlashMode.off);
-			XFile picture = await controller.takePicture();
-			Navigator.push(context, MaterialPageRoute(
-					builder: (context) => DisplayPictureScreen(
-						imagePath: picture,
-					)));
-		} on CameraException catch (e) {
-			debugPrint('Error occured while taking picture: $e');
-			return null;
-		}
+		//server_test();
+		initializeController(); //Camera Controller intialization
 	}
 
 	@override
@@ -81,11 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
 			body: Container(
 				child: Stack(
 					children: [
-						/*VolumeWatcher(
-							onVolumeChangeListener: (double volume) {
-								takePicture();
-							},
-						),*/
 						Transform.scale(
 							scale: scale,
 							alignment: Alignment.topCenter,
@@ -102,10 +67,92 @@ class _MyHomePageState extends State<MyHomePage> {
 			)
 		);
 	}
+
+	Future<void> server_test(XFile file) async {
+		//create multipart request for POST or PATCH method
+		var request = http.MultipartRequest("POST", Uri.parse("http://192.168.1.68:3000/upload"));
+		//add text fields
+		request.fields["text_field"] = "test";
+		//create multipart using filepath, string or bytes
+		var pic = await http.MultipartFile.fromPath("file", file.path);
+		//add multipart to request
+		request.files.add(pic);
+		var response = await request.send();
+
+		//Get the response from the server
+		Uint8List responseData = await response.stream.toBytes();
+
+		Navigator.push(context, MaterialPageRoute(
+				builder: (context) => SentFromServer(
+					responseData: responseData,
+				)));
+
+		//var responseString = String.fromCharCodes(responseData);
+
+		/*final response = await http.get(Uri.parse('http://192.168.1.68:3000/'));
+		if(response.statusCode == 200){
+			debugPrint(response.body);
+		}
+		else{
+			throw Exception('Failed to connect to server');
+		}*/
+	}
+
+        void initializeController(){
+		controller = CameraController(widget.cameras[0], ResolutionPreset.max);
+		controller.initialize().then((_) {
+			if (!mounted) {
+				return;
+			}
+			setState(() {});
+		}).catchError((Object e) {
+			if (e is CameraException) {
+				switch (e.code) {
+					case 'CameraAccessDenied':
+						// Handle access errors here.
+						break;
+					default:
+						// Handle other errors here.
+						break;
+				}
+			}
+		});
+	}	
+
+	Future takePicture() async {
+		if (!controller.value.isInitialized) {return null;}
+		if (controller.value.isTakingPicture) {return null;}
+		try {
+			await controller.setFlashMode(FlashMode.off);
+			XFile picture = await controller.takePicture();
+			server_test(picture);
+			Navigator.push(context, MaterialPageRoute(
+					builder: (context) => DisplayPictureScreen(
+						imagePath: picture,
+					)));
+		} on CameraException catch (e) {
+			debugPrint('Error occured while taking picture: $e');
+			return null;
+		}
+	}
+
 }
+class SentFromServer extends StatelessWidget {
+  const SentFromServer({super.key, required this.responseData});
+  final Uint8List responseData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      body: Image.memory(Uint8List.fromList(responseData)),
+    );
+  }
+}
+
 class DisplayPictureScreen extends StatelessWidget {
   const DisplayPictureScreen({super.key, required this.imagePath});
-  final XFile imagePath;
+  final  imagePath;
 
   @override
   Widget build(BuildContext context) {

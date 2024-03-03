ef0d28a0f2b0082475ea5148e38c655b8e610c7c
Anurag Lamsal <vizculus@gmail.com>
2024-03-03 12:22:58 +0545

    changes

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ Idea Notes:
 * Don't make the camera take the whole screen. Crop it. And put a nicer camera icon. (Done.)
 * Landscape preview is weird. Fix that. (Solved. Just disabled landscape mode.)
 * Make the codebase neater. (It seems like there is no "simple" way to separate logic from UI code, at least for a simple app like ours.)
-* todo: Work more on design. The camera sort of looks weird too when tilting, probably because of height and width. Also, 
+* todo: 
+  # Work more on design. The camera sort of looks weird too when tilting, probably because of height and width. Also, 
   the app glitches if I open the camera app of the phone and switch to our app again. also, using the volume button in the "display picture"
   screen takes pictures -- awkward thing to happen tbh. 
+  # Don't show the picture after it has been taken. Show a progress indicator that indicates waiting for the mp3 to arrive. When the progress
+  indicator is showing, "disable" the "volume click to take pic" functionality (just use a bool flag.) 
+  -- the progress indicator part has been done. make the background opacity low here, and also the disable volume click to take pic functionality
+     is yet to be added. Also, as soon as you click the volume button, do the setState, not when the picture has been officially taken. 
+  # Language choosing option. You can use "volume down" for this btw. 
+
+
+
+
+DO A DESIGN REFINING IN GENERAL WHEN THE FUNCTIONALITY IS GOOD. 
--- lib/home.dart
+++ lib/home.dart
@@ -8,166 +8,172 @@ import 'package:http/src/byte_stream.dart';
 import 'package:volume_watcher/volume_watcher.dart';
 import 'package:flutter/services.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class MyHomePage extends StatefulWidget {
-	MyHomePage({super.key, required this.title, required this.cameras});
+  MyHomePage({super.key, required this.title, required this.cameras});
 
-	String title;
-	List<CameraDescription> cameras;
+  String title;
+  List<CameraDescription> cameras;
 
-	@override
-	State<MyHomePage> createState() => _MyHomePageState();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late CameraController controller;
+  static const _volumeBtnChannel = MethodChannel("mychannel");
+  final player = AudioPlayer();
+  bool _server = false;
 
-	late CameraController controller;
-	static const _volumeBtnChannel =
-			MethodChannel("mychannel");
-	final player = AudioPlayer();
+  @override
+  initState() {
+    super.initState();
 
+    initializeCameraController(); //Camera Controller intialization.
+    initializeVolume(); //Setting the volume to be 1 and customizing the volume buttons to take pictures.
+  }
 
-	@override
-	initState() {
-		super.initState();
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
-		initializeCameraController(); //Camera Controller intialization.
-		initializeVolume(); //Setting the volume to be 1 and customizing the volume buttons to take pictures. 
-	}
+  Widget cameraWidget() {
+    return Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 100),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.65,
+            width: MediaQuery.of(context).size.width,
+            child: CameraPreview(controller),
+          ),
+          SizedBox(height: 30),
+          IconButton.filled(
+            iconSize: 90.0,
+            icon: const Icon(Icons.camera),
+            highlightColor: Colors.blue,
+            onPressed: takePicture,
+          )
+        ]);
+  }
 
-	@override
-	void dispose() {
-		controller.dispose();
-		super.dispose();
-	}
-	
-	@override
-	Widget build(BuildContext context) {
-		if (!controller.value.isInitialized) {
-			return Scaffold(
-				backgroundColor: Color(0xff1c1c1f),
-				body: Center(
-					child: CircularProgressIndicator(
-						color: Colors.white,
-					)
-				)
-			);
-		}
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Scaffold(
+          backgroundColor: Color(0xff1c1c1f),
+          body: Center(
+              child: CircularProgressIndicator(
+            color: Colors.white,
+          )));
+    }
 
-		return Scaffold(
-			backgroundColor: Color(0xff1c1c1f),
-			body: Column(
-				//mainAxisAlignment: MainAxisAlignment.center,
-				//crossAxisAlignment: CrossAxisAlignment.center,
-				children: [
-					SizedBox(height: 100),
-					Container(
-						height: MediaQuery.of(context).size.height * 0.65,
-						width: MediaQuery.of(context).size.width,
-						child: CameraPreview(controller), 
-					),
-					SizedBox(height: 30),
-					IconButton.filled(
-						iconSize: 90.0,
-						icon: const Icon(Icons.camera),
-						highlightColor: Colors.blue,
-						onPressed: takePicture,
-					)
-				]
-			)
-		);
-	}
+    return Scaffold(
+        backgroundColor: Color(0xff1c1c1f),
+        body: ModalProgressHUD(child: cameraWidget(), inAsyncCall: _server));
+  }
 
-	Future<void> initializeVolume() async {
+  Future<void> initializeVolume() async {
+    //Setting volume to max.
+    late var maxVolume;
+    try {
+      maxVolume = await VolumeWatcher.getMaxVolume;
+    } on PlatformException {
+      // handle exception
+    }
 
-		//Setting volume to max. 
-		late var maxVolume; 
-		try{
-			maxVolume = await VolumeWatcher.getMaxVolume;
-		} on PlatformException {
-			// handle exception
-		}
+    //Code to handle what to do when volume keys are pressed. The replacement of default behavior of volume buttons is done in 'MainActivity.kt' in
+    //'android/app/....'.
+    VolumeWatcher.setVolume(maxVolume);
+    _volumeBtnChannel.setMethodCallHandler((call) {
+      if (call.method == "volumeBtnPressed") {
+        if (call.arguments == "volume_down" || call.arguments == "volume_up") {
+          takePicture();
+        }
+      }
 
-		//Code to handle what to do when volume keys are pressed. The replacement of default behavior of volume buttons is done in 'MainActivity.kt' in 
-		//'android/app/....'.
-		VolumeWatcher.setVolume(maxVolume);
-		_volumeBtnChannel.setMethodCallHandler((call) {
-			if (call.method == "volumeBtnPressed") {
-				if (call.arguments == "volume_down" || call.arguments == "volume_up") {
-					takePicture();
-				}
-			}
+      return Future.value(null);
+    });
+  }
 
-			return Future.value(null);
-		});
-	}
+  Future<void> sendRequest(XFile file) async {
+    //create multipart request for POST or PATCH method
+    var request = http.MultipartRequest(
+        "POST", Uri.parse("http://192.168.1.70:3000/upload"));
+    //add text fields
+    request.fields["text_field"] = "test";
+    //create multipart using filepath, string or bytes
+    var pic = await http.MultipartFile.fromPath("file", file.path);
+    //add multipart to request
+    request.files.add(pic);
+    var response = await request.send();
 
+    //Get the response from the server. Currently we only get an audio file corresponding to the tts.
+    Uint8List responseData = await response.stream
+        .toBytes(); //convert the response to bytes such that I can use the response in the app.
+    //Uint8List is preferred for bytes than List.
+    //Play the audio
+    await player.play(BytesSource(responseData));
 
-	Future<void> sendRequest(XFile file) async {
+    setState(() {
+      _server = false;
+    });
+  }
 
-		//create multipart request for POST or PATCH method
-		var request = http.MultipartRequest("POST", Uri.parse("http://192.168.1.70:3000/upload"));
-		//add text fields
-		request.fields["text_field"] = "test";
-		//create multipart using filepath, string or bytes
-		var pic = await http.MultipartFile.fromPath("file", file.path);
-		//add multipart to request
-		request.files.add(pic);
-		var response = await request.send();
+  void initializeCameraController() {
+    controller = CameraController(widget.cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
 
-		//Get the response from the server. Currently we only get an audio file corresponding to the tts. 
-		Uint8List responseData = await response.stream.toBytes(); //convert the response to bytes such that I can use the response in the app.
-	                                                                  //Uint8List is preferred for bytes than List.	
-		//Play the audio
-		await player.play(BytesSource(responseData)); 
-	}
+  Future takePicture() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+    if (controller.value.isTakingPicture) {
+      return null;
+    }
+    try {
+      await controller.setFlashMode(FlashMode.off);
+      XFile picture = await controller.takePicture();
 
-        void initializeCameraController(){
+      sendRequest(picture); //send the picture to the server
 
-		controller = CameraController(widget.cameras[0], ResolutionPreset.max);
-		controller.initialize().then((_) {
-			if (!mounted) {
-				return;
-			}
-			setState(() {});
-		}).catchError((Object e) {
-			if (e is CameraException) {
-				switch (e.code) {
-					case 'CameraAccessDenied':
-						// Handle access errors here.
-						break;
-					default:
-						// Handle other errors here.
-						break;
-				}
-			}
-		});
-	}	
+      setState(() {
+        _server = true;
+      });
 
-	Future takePicture() async {
-
-		if (!controller.value.isInitialized) {return null;}
-		if (controller.value.isTakingPicture) {return null;}
-		try {
-			await controller.setFlashMode(FlashMode.off);
-			XFile picture = await controller.takePicture();
-
-			sendRequest(picture); //send the picture to the server
-
-			Navigator.push(context, MaterialPageRoute(
+      /*Navigator.push(context, MaterialPageRoute(
 					builder: (context) => DisplayPictureScreen(
 						imagePath: picture,
-					)));
-
-		} on CameraException catch (e) {
-			debugPrint('Error occured while taking picture: $e');
-			return null;
-		}
-	}
-
+					)));*/
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
 }
 
-class DisplayPictureScreen extends StatelessWidget {
+/*class DisplayPictureScreen extends StatelessWidget {
   const DisplayPictureScreen({super.key, required this.imagePath});
   final  imagePath;
 
@@ -178,4 +184,4 @@ class DisplayPictureScreen extends StatelessWidget {
       body: Image.file(File(imagePath.path)),
     );
   }
-}
+}*/
